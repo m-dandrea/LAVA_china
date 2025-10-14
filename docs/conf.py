8f316876bcd13721e0c3368a40986d9af5dd88e1
Matteo D'Andrea <teo.dandrea@me.com>
2025-10-14 15:13:23 +0200

    committing documentation draft

--- docs/conf.py
+++ docs/conf.py
@@ -1,16 +1,16 @@
-# Configuration file for the Sphinx documentation builder.
-
-project = 'LAVA_china'
-author = 'm-dandrea'
-release = '0.1'
+project = "LAVA_china"
+author = "m-dandrea"
+release = "0.1"
 
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.viewcode",
 ]
 
-templates_path = ['_templates']
-exclude_patterns = []
+templates_path = ["_templates"]
+html_theme = "sphinx_rtd_theme"
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+root_doc = "index"     # (or master_doc = "index")
+
+# Optional; create docs/_static or comment this out
+html_static_path = ["_static"]
